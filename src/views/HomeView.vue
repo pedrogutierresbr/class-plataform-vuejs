7cd8d3af88255d9cb0a6f4b243f6436d9db7ea2a
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2023-05-18 22:25:00 -0300

    cleaned useless files and added file for information consumption

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">teste</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: "HomeView",
+	};
 </script>
+
+<style></style>
